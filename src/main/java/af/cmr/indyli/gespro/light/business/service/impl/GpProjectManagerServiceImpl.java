0486147895f2520120c13e089ef6dc308fd8567a
sdesousa <sylvaindesousa@free.fr>
2022-01-28 12:19:47 +0100

    technician service

--- src/main/java/af/cmr/indyli/gespro/light/business/service/impl/GpProjectManagerServiceImpl.java
+++ src/main/java/af/cmr/indyli/gespro/light/business/service/impl/GpProjectManagerServiceImpl.java
@@ -16,11 +16,20 @@ public class GpProjectManagerServiceImpl implements IGpProjectManagerService{
 		if (this.empDAO.ifEmpExistByFileNumberOrEmail(emp.getFileNumber(), emp.getEmail(), emp.getLogin())) {
 			throw new GesproBusinessException(String.format("Un employee existe deja avec cet email[%s] ou ce login[%s] ou ce matricule[%s]",emp.getEmail(),emp.getLogin(),emp.getFileNumber()));
 		}
+		if (this.empDAO.ifEmpFieldEmpty(emp.getEmail(), emp.getLogin(), emp.getFileNumber(), emp.getFirstname(), emp.getLastname())) {
+			throw new GesproBusinessException("Email ou login ou matricule ou nom manquant");
+		}
 		return this.empDAO.create(emp);
 	}
 
 	public void update(GpProjectManager emp) throws GesproBusinessException{
-		
+		if (this.empDAO.ifEmpExistByFileNumberOrEmailUpdate(emp.getFileNumber(), emp.getEmail(), emp.getLogin(), emp.getId())) {
+			throw new GesproBusinessException(String.format("Un employee existe deja avec cet email[%s] ou ce login[%s] ou ce matricule[%s]",emp.getEmail(),emp.getLogin(),emp.getFileNumber()));
+		}
+		if (this.empDAO.ifEmpFieldEmpty(emp.getEmail(), emp.getLogin(), emp.getFileNumber(), emp.getFirstname(), emp.getLastname())) {
+			throw new GesproBusinessException("Email ou login ou matricule ou nom manquant");
+		}
+		this.empDAO.update(emp);
 	}
 	
 	public List<GpProjectManager> findAll() {
--- src/test/java/af/cmr/indyli/gespro/light/business/service/test/GpTechnicianServiceTest.java
+++ src/test/java/af/cmr/indyli/gespro/light/business/service/test/GpTechnicianServiceTest.java
@@ -16,134 +16,452 @@ import af.cmr.indyli.gespro.light.business.exception.GesproBusinessException;
 public class GpTechnicianServiceTest {
 
 	private IGpTechnicianService empService = new GpTechnicianServiceImpl();
-	private Integer pmIdForAllTest = null;
-	private Integer createPmId = null;
 	
+
+	private Integer empIdForAllTest = null;
+	private Integer createEmpId = null;
+
 	@Test
-	public void testCreateEmployeeWithSuccess() throws GesproBusinessException {
-		//Given
+	public void testCreateTechnicianWithSuccess() throws GesproBusinessException {
+		// Given
 		GpTechnician emp = new GpTechnician();
 		Assert.assertNull(emp.getId());
-		emp.setFileNumber("1024");
-		emp.setLastname("HOLLANDE");
-		emp.setFirstname("Francois");
-		emp.setPhoneNumber("0365987854");
-		emp.setPassword("mySecondPassword");
-		emp.setEmail("francois.hollande@gouv.fr");
-		emp.setLogin("francois.hollande");
+		emp.setFileNumber("18023");
+		emp.setLastname("PELTIER");
+		emp.setFirstname("Guillaume");
+		emp.setPhoneNumber("0265863289");
+		emp.setPassword("myPassword");
+		emp.setEmail("guillaume.peltier@gouv.fr");
+		emp.setLogin("guillaume.peltier");
 		emp.setLastDiploma("ENA");
 		emp.setGraduationYear(1988);
-		
-		//When
-		emp = empService.create(emp) ;
-		//On le sauvegarde pour le supprimer après
-		this.createPmId = emp.getId();
-		
-		//Then
+
+
+		// When
+		emp = empService.create(emp);
+		//On le sauvegarde pour le supprimer apres
+		this.createEmpId = emp.getId();
+
+		// Then
 		Assert.assertNotNull(emp.getId());
 	}
 	
 	@Test
-	public void testUpdateEmployeeWithSuccess() throws GesproBusinessException {
+	public void testCreateTechnicianWithException() throws GesproBusinessException {
+		// Given
+
+		// When
+		Exception exceptionFileNumber = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("2001");
+			emp.setLastname("Laurent");
+			emp.setFirstname("FABIUS");
+			emp.setPhoneNumber("0125698745");
+			emp.setPassword("myThirdPassword");
+			emp.setEmail("laurent.fabius@gouv.be");
+			emp.setLogin("laurent.fabius.gouv");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			empService.create(emp);
+	    });
+		String actualMessageFileNumber = exceptionFileNumber.getMessage();
+		Exception exceptionEmail = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("2002");
+			emp.setLastname("Laurent");
+			emp.setFirstname("FABIUS");
+			emp.setPhoneNumber("0125698745");
+			emp.setPassword("myThirdPassword");
+			emp.setEmail("laurent.fabius@gouv.fr");
+			emp.setLogin("laurent.fabius.fr");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			empService.create(emp);
+	    });
+		String actualMessageEmail = exceptionEmail.getMessage();
+		Exception exceptionLogin = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("2003");
+			emp.setLastname("Laurent");
+			emp.setFirstname("FABIUS");
+			emp.setPhoneNumber("0125698745");
+			emp.setPassword("myThirdPassword");
+			emp.setEmail("laurent.fabius@gouv.de");
+			emp.setLogin("laurent.fabius");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			empService.create(emp);
+	    });
+		String actualMessageLogin = exceptionLogin.getMessage();
+		Exception exceptionEmptyEmail = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("2002");
+			emp.setLastname("FABIUS");
+			emp.setFirstname("Laurent");
+			emp.setPhoneNumber("0125698745");
+			emp.setPassword("myThirdPassword");
+			emp.setLogin("laurent.fabius.fr");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			empService.create(emp);
+	    });
+		String actualMessageEmptyEmail = exceptionEmptyEmail.getMessage();
+		Exception exceptionEmptyLogin = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("2002");
+			emp.setLastname("FABIUS");
+			emp.setFirstname("Laurent");
+			emp.setPhoneNumber("0125698745");
+			emp.setPassword("myThirdPassword");
+			emp.setEmail("laurent.fabius@gouv.be");
+			emp.setLogin("");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			empService.create(emp);
+	    });
+		String actualMessageEmptyLogin = exceptionEmptyLogin.getMessage();
+		Exception exceptionEmptyFileNumber = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("  ");
+			emp.setLastname("FABIUS");
+			emp.setFirstname("Laurent");
+			emp.setPhoneNumber("0125698745");
+			emp.setPassword("myThirdPassword");
+			emp.setEmail("laurent.fabius@gouv.be");
+			emp.setLogin("laurent.fabius.fr");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			empService.create(emp);
+	    });
+		String actualMessageEmptyFileNumber = exceptionEmptyFileNumber.getMessage();
+		Exception exceptionEmptyFirstname = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("2002");
+			emp.setLastname("FABIUS");
+			emp.setPhoneNumber("0125698745");
+			emp.setPassword("myThirdPassword");
+			emp.setEmail("laurent.fabius@gouv.be");
+			emp.setLogin("laurent.fabius.fr");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			empService.create(emp);
+	    });
+		String actualMessageEmptyFirstname = exceptionEmptyFirstname.getMessage();
+		Exception exceptionEmptyLastname = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("2002");
+			emp.setLastname("");
+			emp.setFirstname("Laurent");
+			emp.setPhoneNumber("0125698745");
+			emp.setPassword("myThirdPassword");
+			emp.setEmail("laurent.fabius@gouv.be");
+			emp.setLogin("laurent.fabius.fr");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			empService.create(emp);
+	    });
+		String actualMessageEmptyLastname = exceptionEmptyLastname.getMessage();
+
+		// Then
+		Assert.assertTrue(actualMessageFileNumber.contains("Un employee existe deja avec cet email"));
+		Assert.assertTrue(actualMessageLogin.contains("Un employee existe deja avec cet email"));
+		Assert.assertTrue(actualMessageEmail.contains("Un employee existe deja avec cet email"));
+		Assert.assertTrue(actualMessageEmptyEmail.contains("Email ou login ou matricule ou nom manquant"));
+		Assert.assertTrue(actualMessageEmptyLogin.contains("Email ou login ou matricule ou nom manquant"));
+		Assert.assertTrue(actualMessageEmptyFileNumber.contains("Email ou login ou matricule ou nom manquant"));
+		Assert.assertTrue(actualMessageEmptyFirstname.contains("Email ou login ou matricule ou nom manquant"));
+		Assert.assertTrue(actualMessageEmptyLastname.contains("Email ou login ou matricule ou nom manquant"));
+	}
+	
+	@Test
+	public void testUpdateTechnicianWithSuccess() throws GesproBusinessException {
 		//Given
 		GpTechnician emp = new GpTechnician();
 		Assert.assertNull(emp.getId());
-		emp.setFileNumber("1024");
-		emp.setLastname("HOLLANDE");
-		emp.setFirstname("Francois");
-		emp.setPhoneNumber("0365987854");
-		emp.setPassword("mySecondPassword");
-		emp.setEmail("francois.hollande@gouv.fr");
-		emp.setLogin("francois.hollande");
+		emp.setFileNumber("18023");
+		emp.setLastname("PELTIER");
+		emp.setFirstname("Guillaume");
+		emp.setPhoneNumber("0265863289");
+		emp.setPassword("myPassword");
+		emp.setEmail("guillaume.peltier@gouv.fr");
+		emp.setLogin("guillaume.peltier");
 		emp.setLastDiploma("ENA");
 		emp.setGraduationYear(1988);
 		emp = empService.create(emp);
-		this.createPmId = emp.getId();
+		this.createEmpId = emp.getId();
+		Assert.assertEquals("18023", emp.getFileNumber());
+		Assert.assertEquals("PELTIER", emp.getLastname());
+		Assert.assertEquals("Guillaume", emp.getFirstname());
+		Assert.assertEquals("0265863289", emp.getPhoneNumber());
+		Assert.assertEquals("myPassword", emp.getPassword());
+		Assert.assertEquals("guillaume.peltier@gouv.fr", emp.getEmail());
+		Assert.assertEquals("guillaume.peltier", emp.getLogin());
+		Assert.assertEquals("ENA", emp.getLastDiploma());
+		Assert.assertEquals(1988, emp.getGraduationYear());
 		
 		//When
-		emp.setFileNumber("U1024");
-		emp.setLastname("UHOLLANDE");
-		emp.setFirstname("UFrancois");
-		emp.setPhoneNumber("U0365987854");
-		emp.setPassword("UmySecondPassword");
-		emp.setEmail("Ufrancois.hollande@gouv.fr");
-		emp.setLogin("Ufrancois.hollande");
-		emp.setLastDiploma("UENA");
-		emp.setGraduationYear(1900);
+		emp.setFileNumber("18888");
+		emp.setLastname("UPELTIER");
+		emp.setFirstname("UGuillaume");
+		emp.setPhoneNumber("U0265863289");
+		emp.setPassword("UmyPassword");
+		emp.setGraduationYear(1992);
 		empService.update(emp);
 		
 		//Then
-		emp = empService.findById(createPmId);
-		Assert.assertEquals("U1024", emp.getFileNumber());
-		Assert.assertEquals("UHOLLANDE", emp.getLastname());
-		Assert.assertEquals("UFrancois", emp.getFirstname());
-		Assert.assertEquals("U0365987854", emp.getPhoneNumber());
-		Assert.assertEquals("UmySecondPassword", emp.getPassword());
-		Assert.assertEquals("Ufrancois.hollande@gouv.fr", emp.getEmail());
-		Assert.assertEquals("Ufrancois.hollande", emp.getLogin());
-		Assert.assertEquals("UENA", emp.getLastDiploma());
-		Assert.assertEquals(1900, emp.getGraduationYear());
+		emp = empService.findById(createEmpId);
+		Assert.assertEquals("18888", emp.getFileNumber());
+		Assert.assertEquals("UPELTIER", emp.getLastname());
+		Assert.assertEquals("UGuillaume", emp.getFirstname());
+		Assert.assertEquals("U0265863289", emp.getPhoneNumber());
+		Assert.assertEquals("UmyPassword", emp.getPassword());
+		Assert.assertEquals("guillaume.peltier@gouv.fr", emp.getEmail());
+		Assert.assertEquals("guillaume.peltier", emp.getLogin());
+		Assert.assertEquals("ENA", emp.getLastDiploma());
+		Assert.assertEquals(1992, emp.getGraduationYear());
 		
 	}
 	
 	@Test
-	public void testFindAllEmployeeWithSuccess() {
-		//Given
+	public void testUpdateTechnicianWithException() throws GesproBusinessException {
+		// Given
+
+		// When
+		Exception exceptionFileNumber = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("18023");
+			emp.setLastname("PELTIER");
+			emp.setFirstname("Guillaume");
+			emp.setPhoneNumber("0265863289");
+			emp.setPassword("myPassword");
+			emp.setEmail("guillaume.peltier@gouv.fr");
+			emp.setLogin("guillaume.peltier");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			emp = empService.create(emp);
+			createEmpId = emp.getId();
+			emp.setFileNumber("2001");
+			empService.update(emp);
+	    });
+		String actualMessageFileNumber = exceptionFileNumber.getMessage();
+		empService.deleteById(createEmpId);
 		
-		//When 
-		List<GpTechnician> emps = this.empService.findAll();
+		Exception exceptionEmail = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("18023");
+			emp.setLastname("PELTIER");
+			emp.setFirstname("Guillaume");
+			emp.setPhoneNumber("0265863289");
+			emp.setPassword("myPassword");
+			emp.setEmail("guillaume.peltier@gouv.fr");
+			emp.setLogin("guillaume.peltier");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			emp = empService.create(emp);
+			createEmpId = emp.getId();
+			emp.setEmail("laurent.fabius@gouv.fr");
+			empService.update(emp);
+	    });
+		String actualMessageEmail = exceptionEmail.getMessage();
+		empService.deleteById(createEmpId);
 		
-		//Then
-		Assert.assertTrue(emps.size() >0);
+		Exception exceptionLogin = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("18023");
+			emp.setLastname("PELTIER");
+			emp.setFirstname("Guillaume");
+			emp.setPhoneNumber("0265863289");
+			emp.setPassword("myPassword");
+			emp.setEmail("guillaume.peltier@gouv.fr");
+			emp.setLogin("guillaume.peltier");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			emp = empService.create(emp);
+			createEmpId = emp.getId();
+			emp.setLogin("laurent.fabius");
+			empService.update(emp);
+	    });
+		String actualMessageLogin = exceptionLogin.getMessage();
+		empService.deleteById(createEmpId);
+		
+		Exception exceptionEmptyEmail = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("18023");
+			emp.setLastname("PELTIER");
+			emp.setFirstname("Guillaume");
+			emp.setPhoneNumber("0265863289");
+			emp.setPassword("myPassword");
+			emp.setEmail("guillaume.peltier@gouv.fr");
+			emp.setLogin("guillaume.peltier");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			emp = empService.create(emp);
+			createEmpId = emp.getId();
+			emp.setEmail(null);
+			empService.update(emp);
+	    });
+		String actualMessageEmptyEmail = exceptionEmptyEmail.getMessage();
+		empService.deleteById(createEmpId);
+		
+		Exception exceptionEmptyLogin = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("18023");
+			emp.setLastname("PELTIER");
+			emp.setFirstname("Guillaume");
+			emp.setPhoneNumber("0265863289");
+			emp.setPassword("myPassword");
+			emp.setEmail("guillaume.peltier@gouv.fr");
+			emp.setLogin("guillaume.peltier");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			emp = empService.create(emp);
+			createEmpId = emp.getId();
+			emp.setLogin("");
+			empService.update(emp);
+	    });
+		String actualMessageEmptyLogin = exceptionEmptyLogin.getMessage();
+		empService.deleteById(createEmpId);
+		
+		Exception exceptionEmptyFileNumber = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("18023");
+			emp.setLastname("PELTIER");
+			emp.setFirstname("Guillaume");
+			emp.setPhoneNumber("0265863289");
+			emp.setPassword("myPassword");
+			emp.setEmail("guillaume.peltier@gouv.fr");
+			emp.setLogin("guillaume.peltier");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			emp = empService.create(emp);
+			createEmpId = emp.getId();
+			emp.setFileNumber(" ");
+			empService.update(emp);
+	    });
+		String actualMessageEmptyFileNumber = exceptionEmptyFileNumber.getMessage();
+		empService.deleteById(createEmpId);
+		
+		Exception exceptionEmptyFirstname = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("18023");
+			emp.setLastname("PELTIER");
+			emp.setFirstname("Guillaume");
+			emp.setPhoneNumber("0265863289");
+			emp.setPassword("myPassword");
+			emp.setEmail("guillaume.peltier@gouv.fr");
+			emp.setLogin("guillaume.peltier");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			emp = empService.create(emp);
+			createEmpId = emp.getId();
+			emp.setFirstname(null);
+			empService.update(emp);
+	    });
+		String actualMessageEmptyFirstname = exceptionEmptyFirstname.getMessage();
+		empService.deleteById(createEmpId);
+		
+		Exception exceptionEmptyLastname = Assert.assertThrows(GesproBusinessException.class, () -> {
+			GpTechnician emp = new GpTechnician();
+			Assert.assertNull(emp.getId());
+			emp.setFileNumber("18023");
+			emp.setLastname("PELTIER");
+			emp.setFirstname("Guillaume");
+			emp.setPhoneNumber("0265863289");
+			emp.setPassword("myPassword");
+			emp.setEmail("guillaume.peltier@gouv.fr");
+			emp.setLogin("guillaume.peltier");
+			emp.setLastDiploma("ENA");
+			emp.setGraduationYear(1988);
+			emp = empService.create(emp);
+			createEmpId = emp.getId();
+			emp.setLastname(null);
+			empService.update(emp);
+	    });
+		String actualMessageEmptyLastname = exceptionEmptyLastname.getMessage();
+		empService.deleteById(createEmpId);
+
+		// Then
+		Assert.assertTrue(actualMessageFileNumber.contains("Un employee existe deja avec cet email"));
+		Assert.assertTrue(actualMessageLogin.contains("Un employee existe deja avec cet email"));
+		Assert.assertTrue(actualMessageEmail.contains("Un employee existe deja avec cet email"));
+		Assert.assertTrue(actualMessageEmptyEmail.contains("Email ou login ou matricule ou nom manquant"));
+		Assert.assertTrue(actualMessageEmptyLogin.contains("Email ou login ou matricule ou nom manquant"));
+		Assert.assertTrue(actualMessageEmptyFileNumber.contains("Email ou login ou matricule ou nom manquant"));
+		Assert.assertTrue(actualMessageEmptyFirstname.contains("Email ou login ou matricule ou nom manquant"));
+		Assert.assertTrue(actualMessageEmptyLastname.contains("Email ou login ou matricule ou nom manquant"));
 	}
-	
+
+	@Test
+	public void testFindAllTechnicianWithSuccess() {
+		// Given
+
+		// When
+		List<GpTechnician> emps = this.empService.findAll();
+		// Then
+		Assert.assertTrue(emps.size() > 0);
+	}
+
 	@Test
 	public void testFindByIdWithSuccess() {
-		//Given
-		Integer empId = this.pmIdForAllTest;
-		
-		//When 
+		// Given
+		Integer empId = this.empIdForAllTest;
+
+		// When
 		GpTechnician emp = this.empService.findById(empId);
-		//Then
+		// Then
 		Assert.assertNotNull(emp);
 	}
-	
+
 	@Test
-	public void testDeleteByIdWithSuccess() {
-		//Given
-		Integer empId = this.pmIdForAllTest;
-		
-		//When 
+	public void testDelete() {
+		// Given
+		Integer empId = this.empIdForAllTest;
+
+		// When
 		this.empService.deleteById(empId);
-		
-		
-		//Then
 		GpTechnician emp = this.empService.findById(empId);
+
+		// Then
 		Assert.assertNull(emp);
 	}
-	
+
 	@Before
 	public void prepareAllEntityBefore() throws GesproBusinessException {
 		GpTechnician emp = new GpTechnician();
 		Assert.assertNull(emp.getId());
-		emp.setFileNumber("1050");
-		emp.setLastname("Segolene");
-		emp.setFirstname("ROYAL");
-		emp.setPhoneNumber("0256897856");
-		emp.setPassword("mySecondPassword");
-		emp.setEmail("segolene.royal@gouv.fr");
-		emp.setLogin("sego.royal");
+		emp.setFileNumber("2001");
+		emp.setLastname("FABIUS");
+		emp.setFirstname("Laurent");
+		emp.setPhoneNumber("0125698745");
+		emp.setPassword("myThirdPassword");
+		emp.setEmail("laurent.fabius@gouv.fr");
+		emp.setLogin("laurent.fabius");
 		emp.setLastDiploma("ENA");
-		emp.setGraduationYear(1990);
+		emp.setGraduationYear(1988);
 		emp = empService.create(emp) ;
-		this.pmIdForAllTest = emp.getId();
+		this.empIdForAllTest = emp.getId();
 	}
-	
+
 	@After
 	public void deleteAllEntityAfter() {
-		this.empService.deleteById(this.pmIdForAllTest);
-		if(!Objects.isNull(this.createPmId)) {
-			this.empService.deleteById(this.createPmId);
+		this.empService.deleteById(this.empIdForAllTest);
+		if(!Objects.isNull(this.createEmpId)) {
+			this.empService.deleteById(this.createEmpId);
 		}
 	}
 }
